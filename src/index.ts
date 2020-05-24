5cf78788399aef704ec5d495e974e10a5ee4d21b
aelbore <aelbore.sg@gmail.com>
2020-05-24 19:52:38 +0800

    refactor code add path resolver

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,12 @@
 import {Options, transform} from '@swc/core'
-import {existsSync, statSync} from 'fs'
-import {dirname, join} from 'path'
 import {Plugin} from 'rollup'
+import {pathResolver} from './path-resolver'
 
 type PluginOptions<O = Options> = Pick<O, Exclude<keyof O, 'filename'>>
 
-const extensions = [ 'ts', 'js', 'tsx', 'jsx' ]   
-
-const resolveFile = (resolved: string, index: boolean = false) => {
-  for (const extension of extensions) {
-    const file = index 
-      ? join(resolved, `index.${extension}`)
-      : `${resolved}.${extension}`
-    if (existsSync(file)) {
-      return file 
-    }
-  }
-  return null
-}
-
 export default (options: PluginOptions = {}): Plugin => ({
   name: 'swc',
-  resolveId(id, origin) {
-    if (!origin) {
-      return id 
-    }
-    const resolved = join(dirname(origin), id)
-    const file = resolveFile(resolved)
-    if (file) {
-      return file 
-    }
-    if (existsSync(resolved) && statSync(resolved).isDirectory()) {
-      const coreFile = resolveFile(resolved, true)
-      if (coreFile) {
-        return coreFile 
-      }
-    }
-    return null
-  },
+  resolveId: pathResolver(),
   transform(code, filename) {
     (options as PluginOptions & {filename: string}).filename = filename
     return transform(code, options)
