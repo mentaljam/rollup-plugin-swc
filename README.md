78ac9294452fbbfb7f2093b83093c80989c05463
Manoj Vivek <p.manoj.vivek@gmail.com>
2021-11-10 17:11:58 +0530

    Added support for include and exclude options to have more control over the build files that get transpiled with swc

--- README.md
+++ README.md
@@ -25,6 +25,9 @@ export default {
   },
   plugins: [
     swc({
+      rollup: {
+        exclude: 'path/to/exclude/',
+      },
       jsc: {
         parser: {
           syntax: 'typescript',
@@ -38,7 +41,23 @@ export default {
 
 ## Options
 
-The plugin takes all the [SWC options](https://swc-project.github.io/docs/configuring-swc) except the `filename`.
+The plugin takes all the [SWC options](https://swc-project.github.io/docs/configuring-swc) except the `filename`. 
+
+In addition to the above `SWC Options`, it takes following options for smoother integration with the `rollup` plugin convention:
+
+### `rollup.exclude`
+
+Type: `String` | `Array[...String]`<br>
+Default: `null`
+
+A [minimatch pattern](https://github.com/isaacs/minimatch), or array of patterns, which specifies the files in the build the plugin should _ignore_. By default no files are ignored.
+
+### `rollup.include`
+
+Type: `String` | `Array[...String]`<br>
+Default: `null`
+
+A [minimatch pattern](https://github.com/isaacs/minimatch), or array of patterns, which specifies the files in the build the plugin should operate on. By default all files are targeted.
 
 ## License
 
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,34 @@
 import {Options, transform} from '@swc/core'
 import {Plugin} from 'rollup'
+import {createFilter, FilterPattern} from '@rollup/pluginutils';
 
-type PluginOptions<O = Options> = Pick<O, Exclude<keyof O, 'filename'>>
+type SWCPluginOptions<O = Options> = Pick<O, Exclude<keyof O, 'filename'>>;
 
-type RollupPluginSWC = (options?: PluginOptions) => Plugin
+type RollUpOptions = {
+  include: FilterPattern;
+  exclude: FilterPattern;
+};
 
-const swc: RollupPluginSWC = (options = {}) => ({
-  name: 'swc',
-  transform(code, filename) {
-    (options as PluginOptions & {filename: string}).filename = filename
-    return transform(code, options)
-  }
-})
+type PluginOptions = SWCPluginOptions & {rollup?: RollUpOptions};
+
+type RollupPluginSWC = (options?: PluginOptions) => Plugin;
+
+const swc: RollupPluginSWC = (pluginOptions = {}) => {
+  const {rollup, ...options} = pluginOptions;
+
+  const filter = createFilter(rollup?.include, rollup?.exclude);
+
+  return {
+    name: 'swc',
+    transform(code, filename) {
+      if (!filter(filename)) {
+        return null;
+      }
+
+      (options as SWCPluginOptions & {filename: string}).filename = filename;
+      return transform(code, options);
+    },
+  };
+};
 
 export default swc
